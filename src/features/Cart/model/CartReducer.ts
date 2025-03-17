e2a5ce3645c432103ca61eabf18791cd306a8c23
«Elena <LenaRybinskova@gmail.com>
2025-03-17 23:58:55 +0300

    SCRUM-2 add: add BookCard, Modal component

--- src/features/Cart/model/CartReducer.ts
+++ src/features/Cart/model/CartReducer.ts
@@ -1,8 +1,19 @@
+import {BookApp} from '@/app/appReducer.ts';
+
 const initialState = {
-    cart: [] ,
+    cart: {} as Record<number, BookApp>,
 }
 
 export type initialStateType = typeof initialState;
+
+/*
+const init state = {
+    cart:{
+    "1":{userId: 1, id: 1, title: 'sunt', body: 'quia et o', author: 'Sarah Connor', …},
+    "2":{userId: 1, id: 2, title: 'qui est esse', body: 'esi nulla', author: 'Sarah Connor'}
+}}
+*/
+
 
 const ADD_BOOK = 'ADD_BOOK'
 const DELETE_BOOK = 'DELETE_BOOK'
@@ -11,24 +22,31 @@ export const cartReducer = (state: initialStateType = initialState, action: any)
     switch (action.type) {
         case ADD_BOOK:
             return {
-                ...state}
+                ...state, cart: {
+                    ...state.cart, [action.payload.id]: action.payload,
+                },
+            }
+        case DELETE_BOOK:
+            const updatedCart = { ...state.cart }
+            delete updatedCart[action.payload];
+            return {...state, cart: updatedCart};
         default:
             return state
     }
 }
 
 
-export const addBookAC = (book: any) => {
+export const addBookAC = (book: BookApp) => {
     return {
         type: ADD_BOOK,
         payload: book
     } as const
 }
 
-export const deleteBookAC = (book: any) => {
+export const deleteBookAC = (bookId: number) => {
     return {
         type: DELETE_BOOK,
-        payload: book
+        payload: bookId
     } as const
 }
 
@@ -37,16 +55,4 @@ export type AddBook = ReturnType<typeof addBookAC>
 export type DeleteBook = ReturnType<typeof deleteBookAC>
 export type CartActions = AddBook | DeleteBook
 
-/*export const fetchBooksData = () => async (dispatch: any) => {
-    dispatch(setAppStatusAC('loading'))
-    return authApi.getBooks()
-        .then(data => {
-            dispatch(setBookAC(data))
-            dispatch(setAppStatusAC('succeeded'))
-        })
-        .catch(() => {
-                dispatch(setAppStatusAC('failed'))
-                throw new Error('Failed to fetch info')
-            }
-        )
-}*/
+
